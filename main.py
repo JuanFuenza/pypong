322ef6136c10fa50a235f7b82baa3da39f1d27cb
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-09-12 16:08:10 -0300

    feat: music, sfx and start menu

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ class Ball():
         self.move()
     
     def __del__(self):
-        print("score")
+        pass
 
 # init pygame
 pygame.init()
@@ -108,8 +108,19 @@ pygame.display.set_caption("Pygame!")
 FPS = 60
 clock = pygame.time.Clock()
 
+# play
+play = False
+
 # fonts
 font = pygame.font.Font("assets/fonts/VCR_OSD_MONO_1.001.ttf", 20)
+game_font = pygame.font.Font("assets/fonts/VCR_OSD_MONO_1.001.ttf", 120)
+
+# texts
+start_game = game_font.render("PYPONG", False, "white")
+start_game_rect = start_game.get_rect(center = (WIDTH//2, 150))
+
+new_game = font.render("Press space to start", False, "white")
+new_game_rect = new_game.get_rect(center = (WIDTH//2, 400))
 
 # keys
 # player 1
@@ -133,45 +144,71 @@ ball = Ball()
 s_1 = 0
 s_2 = 0
 
+# sounds
+bounce_sfx = pygame.mixer.Sound("assets/sfx/bounce.wav")
+bounce_sfx.set_volume(0.1)
+
+score_sfx = pygame.mixer.Sound("assets/sfx/score.wav")
+score_sfx.set_volume(0.1)
+
+# Init music
+pygame.mixer.init()
+pygame.mixer.music.load("assets/music/music.mp3")
+pygame.mixer.music.set_volume(0.05)
+pygame.mixer.music.play(loops=-1)
+
 # game main loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if not play:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    play = True
+                    pygame.mixer.music.set_volume(0.10)
     
     # fill
     ds.fill((0, 0, 0))
 
-    ball.draw(ds)
-    ball.update()
+    if not play:
+        ds.blit(start_game, start_game_rect)
+        ds.blit(new_game, new_game_rect)
+    else:
+        ball.draw(ds)
+        ball.update()
 
-    # player
-    for p in players:
-        p.draw(ds)
-        p.update()
+        # player
+        for p in players:
+            p.draw(ds)
+            p.update()
 
-        # collision
-        collision(p)
+            # collision
+            collision(p)
 
-        # score
-        score(s_1, 50)
-        score(s_2, 740)
-    
-        # ball collision
-        collision_tolerance = 20
-        if ball.rect.colliderect(p.rect): 
-            if int(p.rect.right - ball.rect.left) <= collision_tolerance and ball.vel_x < 0 or int(p.rect.left - ball.rect.right) <= collision_tolerance and ball.vel_x > 0:
-                ball.vel_x *= -1
-                ball.vel_x = round(ball.vel_x * 1.2)
-    
-    if ball.rect.x > WIDTH + 100:
-        get_new_ball(ball)
-        s_1 += 1
+            # score
+            score(s_1, 50)
+            score(s_2, 740)
+        
+            # ball collision
+            collision_tolerance = 20
+            if ball.rect.colliderect(p.rect): 
+                if int(p.rect.right - ball.rect.left) <= collision_tolerance and ball.vel_x < 0 or int(p.rect.left - ball.rect.right) <= collision_tolerance and ball.vel_x > 0:
+                    ball.vel_x *= -1
+                    ball.vel_x = round(ball.vel_x * 1.2)
+                    bounce_sfx.play()
+        
+        if ball.rect.x > WIDTH + 100:
+            get_new_ball(ball)
+            s_1 += 1
+            score_sfx.play()
 
-    if ball.rect.x < 0 - 100:
-        get_new_ball(ball)
-        s_2 += 1
+        if ball.rect.x < 0 - 100:
+            get_new_ball(ball)
+            s_2 += 1
+            score_sfx.play()
 
 
     # update
